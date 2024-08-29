ce7a4bdcabc3b104e2a80d24082893b21f38be05
Luis Henrique <luishenriquealbino@gmail.com>
2024-08-28 21:11:25 -0300

    KTor

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.clankypants.financial-healthy-life-api"
---
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -0,0 +1,17 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import java.io.File
+
+fun Application.configureSecurity() {
+//    install(Authentication) {
+////        firebase {
+////            adminFile = File("path/to/admin/file.json")
+////            realm = "My Server"
+////            validate { token ->
+////                MyAuthenticatedUser(id = token.uid)
+////            }
+//        }
+//    }
+}
